218c4bdb430a68e637e1792729079c89997c2060
Karl Pasciak <pasciakarl@gmail.com>
2018-07-26 11:04:13 -0400

    changed path in pom.xml

--- src/main/Java/com/revature/model/dao/implementations/UserDaoImplHibernate.java
+++ src/main/Java/com/revature/model/dao/implementations/UserDaoImplHibernate.java
@@ -2,13 +2,11 @@ package com.revature.model.dao.implementations;
 
 import com.revature.model.beans.User;
 import com.revature.model.dao.interfaces.UserDao;
-import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
