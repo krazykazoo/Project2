90fd5837158c5dcffc80d9f9d36c4d6dd5e25061
Karl Pasciak <pasciakarl@gmail.com>
2018-07-25 20:15:37 -0400

    User Controller Created

--- src/main/Java/com/revature/controller/UserController.java
+++ src/main/Java/com/revature/controller/UserController.java
@@ -1,22 +1,45 @@
 package com.revature.controller;
 
+import com.revature.model.beans.User;
+import com.revature.model.beans.meta.UserPass;
 import com.revature.service.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@ResponseBody
-@RequestMapping(path="")
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+@RequestMapping(path="users")
 public class UserController {
 
-    UserServices userServices;
+    private UserServices userServices;
 
     @Autowired
     public void setUserServices(UserServices userServices) {
         this.userServices = userServices;
     }
 
-
+    @PostMapping(path = "/login", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<User> authenticate(@RequestBody UserPass userPass, HttpServletResponse response) {
+        User user = userServices.authenticate(userPass.getUsername(),userPass.getPassword());
+        if (user != null) {
+            Cookie cookie = new Cookie("id", user.getId().toString());
+            response.addCookie(cookie);
+            return new ResponseEntity<User>(user, HttpStatus.OK);
+        }
+        else {
+            try {
+                response.sendError(401);
+                return null;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
 }
---
+++ src/main/Java/com/revature/model/beans/meta/UserPass.java
@@ -0,0 +1,39 @@
+package com.revature.model.beans.meta;
+
+
+public class UserPass {
+    private String username;
+    private String password;
+
+    public UserPass() {
+    }
+
+    public UserPass(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserPass{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
